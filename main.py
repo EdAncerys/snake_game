80ac39231b08cc620d2fe68e25eeb941f21087dd
Ed <lookatemail@gmail.com>
2021-09-27 09:28:13 +0100

    Add movement to snake - bale to move forward

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 
 from turtle import Screen, Turtle
+import time
 
 # screen env setup
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Snake Game')
+screen.tracer(0) #enable animation
 
 # snake object
-segment_1 = Turtle('square')
-segment_1.color('white')
+segments = []
+starting_position = [(0,0), (-20,0), (-40, 0)]
+for position in starting_position:
+    new_segment = Turtle('square')
+    new_segment.color('white')
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
 
-segment_2 = Turtle('square')
-segment_2.color('white')
-segment_2.goto(-20, 0)
 
-segment_3 = Turtle('square')
-segment_3.color('white')
-segment_3.goto(-40, 0)
-
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num -1].xcor()
+        new_y = segments[seg_num -1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
 
 screen.exitonclick()
