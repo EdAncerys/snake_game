d2bf829fc545bf5e3e5e057fc5a3ca87d7e89120
Ed <lookatemail@gmail.com>
2021-09-27 09:47:56 +0100

    Extract snake to seperate class

--- main.py
+++ main.py
@@ -1,33 +1,21 @@
-
-from turtle import Screen, Turtle
+from turtle import Screen
+from snake import Snake
 import time
 
-# screen env setup
 screen = Screen()
 screen.setup(width=600, height=600)
-screen.bgcolor('black')
-screen.title('Snake Game')
-screen.tracer(0) #enable animation
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
 
-# snake object
-segments = []
-starting_position = [(0,0), (-20,0), (-40, 0)]
-for position in starting_position:
-    new_segment = Turtle('square')
-    new_segment.color('white')
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
-
+snake = Snake()
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num -1].xcor()
-        new_y = segments[seg_num -1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
+
+    snake.move()
+
 
 screen.exitonclick()
